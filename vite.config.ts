f7d13d5c6ee8780a4b5dc9ac4a11d3e0535173cb
Stiven Antequera Ferrer <90526249+santequera1@users.noreply.github.com>
2025-11-20 11:33:31 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,11 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
       },
+      preview: {
+        port: 3000,
+        host: '0.0.0.0',
+        allowedHosts: ['taskappdtgrowthpartners.onrender.com']
+      },
       plugins: [react()],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
